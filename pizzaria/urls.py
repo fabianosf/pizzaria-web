cb14aff393e4440be7437e46bbfc2d2392a85106
Fabiano Freitas <fabiano.freitas@gmail.com>
2022-02-21 22:27:01 -0300

    Implementando login e logout

--- pizzaria/urls.py
+++ pizzaria/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('core.urls')),
     path('cliente/', include('cliente.urls')),
     path('admin/', admin.site.urls),
-    
-    
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
